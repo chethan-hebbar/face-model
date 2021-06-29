2584dac5f913a4cf631923135c0fefc2baaa4d5f
chethan-hebbar <chethan0hebbar@gmail.com>
2021-06-29 09:39:43 +0530

    Major update

--- app.js
+++ app.js
@@ -1,47 +1,84 @@
-function main(){
-  // creating the camera
-  var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-  camera.position.set(0, 0, 200);
 
-  // creating the renderer and inserting it into the html file
-  var renderer = new THREE.WebGLRenderer();
-  renderer.setSize(window.innerWidth, window.innerHeight);
+let leftEye, rightEye, scene, renderer, camera;
+
+init();
+function init(){
+  const modelPath = "https://d1a370nemizbjq.cloudfront.net/a7331c3f-0dc7-431c-88fa-5dfc951c92db.glb";
+  scene = new THREE.Scene();
+  scene.background = new THREE.Color(0xfde2af);
+
+  renderer = new THREE.WebGLRenderer({canvas: document.querySelector("#c"), antialias: true});
+  renderer.setPixelRatio(window.devicePixelRatio);
   document.body.appendChild(renderer.domElement);
 
-  // creating the scene
-  var scene = new THREE.Scene();
-  var ambientLight = new THREE.ambientLight(0xfffff);
-  scene.add(ambientLight);
+  camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 2000);
+  camera.position.set(0, 4.6, 3);
 
-  // loading the model along with the materials 
-  var mtlLoader = new THREE.MTLLoader();
-  mtlLoader.setBaseUrl( 'IronMan/' );
-  mtlLoader.setPath( 'IronMan/' );
-  var url = "IronMan.mtl";
-  mtlLoader.load( url, function( materials ) {
+  var loader = new THREE.GLTFLoader();
+  loader.load(
+    modelPath,
+    function(gltf){
+      model = gltf.scene;
+      model.traverse(function(child){
+        if(child.isMesh){
+          child.castShadow = true;
+          child.recieveShadow = true;
+        }
 
-    materials.preload();
+        if(child.name === 'LeftEye' && child.isBone){
+          child.rotateZ(Math.PI / 6);
+          leftEye = child;
+        }
 
-    var objLoader = new THREE.OBJLoader();
-    objLoader.setMaterials( materials );
-    objLoader.setPath( 'IronMan/' );
-    objLoader.load('IronMan.obj', function (object) {
+        if(child.name === 'RightEye' && child.isBone){
+          rightEye = child;
+        }
 
-        object.position.y = - 95;
-        scene.add( object );
-        var loader = new THREE.OBJLoader();
-        loader.addEventListener('load', function (event){
-          scene.add(event.content);
-        });
+        console.log(child);
+      });
 
-        renderer.render(scene, camera);
-    }, 
-    undefined, 
+      model.scale.set(8, 8, 8);
+      scene.add(model);
+    },
+    undefined,
     function(err){
       console.error(err);
-    });
+    }
+  );
 
-  });
+  scene.add(new THREE.AmbientLight(0xffffff));
 }
 
-main();
+function update(){
+  if (resizeRendererToDisplaySize(renderer)){
+    const canvas = renderer.domElement;
+    camera.aspect = canvas.clientWidth / canvas.clientHeight;
+    camera.updateProjectionMatrix();
+  }
+
+  requestAnimationFrame(update);
+  renderer.render(scene, camera);
+}
+update();
+
+function resizeRendererToDisplaySize(renderer){
+  const canvas = renderer.domElement;
+  let width = window.innerWidth;
+  let height = window.innerHeight;
+  let canvasPixelWidth = canvas.width / window.devicePixelRatio;
+  let canvasPixelHeight = canvas.height / window.devicePixelRatio;
+
+  const needResize =
+    canvasPixelWidth !== width || canvasPixelHeight !== height;
+  if (needResize){
+    renderer.setSize(width, height, false);
+  }
+  return needResize;
+}
+
+function moveParts(leftEye, rightEye){
+  if(leftEye){
+    leftEye.rotateZ(Math.PI / 6);
+  }
+}
+moveParts(leftEye, rightEye);
